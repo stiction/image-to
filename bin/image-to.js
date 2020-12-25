a16934dd4726a664c2aa20d6a44d69018949e31f
stiction <258757028@qq.com>
2020-12-25 19:01:26 +0800

    show image info

--- bin/image-to.js
+++ bin/image-to.js
@@ -2,19 +2,30 @@
 
 const sharp = require('sharp');
 
+function printHelp() {
+    console.log('arguments: srcFile dstFile');
+    console.log('supports reading JPEG, PNG, WebP, TIFF, GIF and SVG images.');
+    console.log('output images can be in JPEG, PNG, WebP and TIFF formats.');
+}
+
 const main = async () => {
-    if (process.argv.length < 4) {
-        console.log('arguments: srcFile dstFile');
-        console.log('supports reading JPEG, PNG, WebP, TIFF, GIF and SVG images.');
-        console.log('output images can be in JPEG, PNG, WebP and TIFF formats.');
+    if (process.argv.length < 3) {
+        printHelp();
         return;
     }
 
-    const srcFile = process.argv[2];
-    const dstFile = process.argv[3];
-
     try {
-        const info = await sharp(srcFile).toFile(dstFile);
+        const srcFile = process.argv[2];
+        const image = sharp(srcFile);
+        const meta = await image.metadata();
+        console.log(meta);
+        if (process.argv.length < 4) {
+            return;
+        }
+
+        const dstFile = process.argv[3];
+        const info = await image.toFile(dstFile);
+        console.log('---------- target ----------');
         console.log(info);
     } catch (err) {
         console.error(err);
